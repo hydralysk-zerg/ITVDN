14c1663374ff961c55930ddb8eff3f4bf6326357
Oleksii <aleksey.ilchenko.m@gmail.com>
2022-04-12 16:41:15 +0300

    Start lession 3

--- Python_basic/lession2/lession_2_encapsulation.py
+++ Python_basic/lession2/lession_2_encapsulation.py
@@ -44,7 +44,6 @@
 # print(user.name)
 
 # class Worker:
-#     RIGHTS = 'Equal'
 #     SALARY_CLASS = {
 #             'A': 100,
 #             'B': 200,
@@ -66,9 +65,51 @@
 
 # ------------------------- Task example ---------------------------
 
-class TextProcessor:
-    pass
+# class TextProcessor:
+#     def __init__(self):
+#         self._punctuation = ",#*&?;:`~"
 
+#     def __is_punctuation(self, char):
+#         return char in self._punctuation
+
+#     def get_clean_string(self, text):
+#         clean_text = ''
+#         for char in text:
+#             if self.__is_punctuation(char):
+#                 continue
+#             clean_text += char
+#         return clean_text
+
+
+# class TextLoader:
+#     def __init__(self):
+#         self.__text_processor = TextProcessor()
+#         self.__clean_string = None
+
+#     def set_clean_string(self, text):
+#         self.__clean_string = self.__text_processor.get_clean_string(text)
+
+#     @property
+#     def clean_string(self):
+#         print(f'Clean string is:{self.__clean_string}')
+#         return self.__clean_string
+
+
+# class DataInterface:
+#     def __init__(self) -> None:
+#         self.__text_loader = TextLoader()
+
+#     def process_texts(self, texts):
+#         clean_texts = []
+#         for text in texts:
+#             self.__text_loader.set_clean_string(text)
+#             clean = self.__text_loader.clean_string
+#             clean_texts.append(clean)
+#         return clean_texts
+
+# p = DataInterface()
+# t = ['Hello, I am John', 'Hey, what is the weather today?']
+# p.process_texts(t)
 
 # ------------------------- Homework --------------------------------
 # 1. Написать класс, который описывает пользователя (class User), сделать ему приватный
@@ -76,3 +117,28 @@ class TextProcessor:
 # передается в конструктор.
 # 2. Написать getter и setter для атрибута age.
 # 3. Добавить в setter проверку на валидный возраст (не отрицательное, целое число).
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, value):
+        if value == int(value):
+            if value > 0:
+                self.__age = value
+                print(f'I am {self.name} I am age {self.age}')
+            else:
+                print(f'Age {self.name} cannot be less than zero')
+        else:
+            print(f'Age {self.name} not integer')
+
+
+u = User(age=0, name='Ivan')
+u.age = 34
+print(u.age, u.name)
