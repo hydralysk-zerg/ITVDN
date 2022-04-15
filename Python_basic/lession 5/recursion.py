c685e7761bd66f0d7aae6d0ae89274a24a17de58
Oleksii <aleksey.ilchenko.m@gmail.com>
2022-04-15 16:34:56 +0300

    Start lession 5 recursion

---
+++ Python_basic/lession 5/recursion.py
@@ -0,0 +1,71 @@
+# ---------------------------------- recursion -------------------------------------
+# a = [1, 2, 3, 4, 5]
+# b = [56, 67, 3, 78, 890, 2]
+
+# def iter_arr(array):
+#     for element in array:
+#         print(element)
+
+# def iter_rec(array, index=0):
+#     if index < len(array):
+#         print(array[index])
+#         index += 1
+#         iter_rec(array, index)
+
+# iter_rec(b)
+# iter_arr(a)
+
+class Node:
+    def __init__(self, data) -> None:
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def search(self, data):
+        if data == self.data:
+            print(f'{data} in tree')
+            return True
+        if data < self.data:
+            if self.left is None:
+                print(f'{data} not in tree')
+                return False
+            return self.left.search(data)
+        if self.right is None:
+            print(f'{data} not in tree')
+            return False
+        return self.right.search(data)
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data)
+        if self.right:
+            self.right.print_tree()
+        # print(self.data)
+
+a = [23, 34, 19, 89, 987, 890, 45, 56]
+for i in range(len(a)):
+    if i == 0:
+        root = Node(a[i])
+    else:
+        root.insert(a[i])
+root.search(34)
+
+
+
+
